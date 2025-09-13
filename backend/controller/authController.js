b811d338330fe65ba2163fafa6e69efbbbce11e8
Divy Anant Varshney <divyanantx@gmail.com>
2025-09-13 20:48:14 +0530

    create routes

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,9 +1,11 @@
 import User from "../model/userModel.js";
 import validator from "validator"
 import bcrypt from "bcryptjs"
+import { genToken } from "../config/token.js";
+import { log } from "console";
 
 
-export const register = async (req, res) => {
+export const registration = async (req, res) => {
     try {
         const { name, email, password } = req.body;
         const existUser = await User.findOne({ email })
@@ -19,11 +21,56 @@ export const register = async (req, res) => {
             return res.status(400).json({ message: "Enter Strong Password" })
         }
 
-        let hashPassword = await bcrypt.hash(password,10);
+        let hashPassword = await bcrypt.hash(password, 10);
 
-        const user=await User.create({name,email,password:hashPassword});
+        const user = await User.create({ name, email, password: hashPassword });
+        let token = await genToken(user._id)
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        return res.status(201).json(user);
 
     } catch (error) {
+        console.log("register error");
+        return res.status(500).json({ message: `Registration error ${error}` })
+    }
+}
 
+export const login = async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        let isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(404).json({ message: "Incorrect password" });
+        }
+        let token = await genToken(user._id)
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        return res.status(201).json({ message: "login successful" });
+    } catch (error) {
+        console.log("login error");
+        return res.status(500).json({ message: `Login error ${error}` })
+    }
+}
+
+export const logOut = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({ message: "LogOut successful" });
+
+    } catch (error) {
+         console.log("logOut error");
+        return res.status(500).json({ message: `LogOut error ${error}` })
     }
 }
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema({
     }
 } ,{timeStamp:true ,minimize:false})
 
-const User =mongoose.Model("User",userSchema);
+const User =mongoose.model("User",userSchema);
 
 export default User
