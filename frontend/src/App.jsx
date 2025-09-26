960b02acee0bd7efe433601f3ada67ea029c35ef
Divy Anant Varshney <divyanantx@gmail.com>
2025-09-26 23:58:38 +0530

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Registration from './pages/Registration'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Registration />} />
+        
+      </Routes>
+    </>
   )
 }
 
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
 
 function Registration() {
+  let navigate=useNavigate()
   return (
-    <div>
-        
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-white flex flex-col items-center justify-start">
+      <div className='w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px] cursor-pointer' onClick={()=>navigate("/")}>
+          <img className='w-[40px]' src={Logo} alt="" />
+          <h1 className='text-[22px] font-sans'>OneCart</h1>
+      </div>
+      <div></div>
     </div>
+
   )
 }
 
