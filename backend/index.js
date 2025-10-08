88e963c3a6b7272351c621e664ff344996cb94bc
Divy Anant Varshney <divyanantx@gmail.com>
2025-10-08 15:18:44 +0530

    add files

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import connectDb from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
-
+import cors from "cors"
 dotenv.config()
 
 let port = process.env.PORT || 6000;
@@ -12,15 +12,19 @@ let app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes)
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello from server");
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Hello from server");
     connectDb()
 })
